97f9756a4c77481529aab5f7085399220acff5f9
Cedric <artigue.cedric@gmail.com>
2025-01-06 14:47:49 +0100

    add full api integration test

--- api/tests/integration/test_api.py
+++ api/tests/integration/test_api.py
@@ -12,18 +12,41 @@ from api.domain.repositories import ITodoRepository
 from api.infrastructure.database.client import SQL_BASE, get_engine
 from api.interface_adapters.gateways.todo import InMemoryTodoRepository, SQLTodoRepository
 
+@pytest.fixture(scope="module")
+def client():
+    with TestClient(app) as c:
+      yield c
+
+@pytest.fixture(scope="module")
+def test_user():
+    return {"username": "johndoe", "password": "secret"}
+
+def test_login(client, test_user):
+  response = client.post("/login", data=test_user)
+  assert response.status_code == 200
+  token = response.json()["access_token"]
+  assert token is not None
+  return token
+
 @pytest.mark.integration
-def test_api():
+def test_api(client, test_user):
     time.sleep(1)
-    assert True
-    # client = TestClient(app)
+    # test 404, page does not exist
+    response = client.get("/fail")
+    assert response.status_code == 404
 
-    # response = client.post("/create/lo?value=testvalue")
-    # assert response.status_code == 201
+    # test_login and retrieve token for subsequent authenticated routes tests
+    token = test_login(client, test_user)
 
-    # response = client.get("/get/testkey")
-    # assert response.status_code == 200
-    # assert response.json() == {"key": "testkey", "value": "testvalue", "done": False}
+    # verify that user is loggedin
+    response = client.get("/users/me", headers={"Authorization": f"Bearer {token}"})
+    assert response.status_code == 200
+    me = response.json()
+    assert me['username'] == 'johndoe'
 
-    # response = client.get("/get/wrong")
-    # assert response.status_code == 404
+    # verify that user is loggedin
+    response = client.get("/repos/MergifyIO/mergify-cli/starneighbours", headers={"Authorization": f"Bearer {token}"})
+    assert response.status_code == 200
+    neighbours = response.json()
+    assert neighbours is not None
+ 
