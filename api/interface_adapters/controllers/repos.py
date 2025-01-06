785c0402f861906d242e708539f907c79005131f
Cedric <artigue.cedric@gmail.com>
2025-01-06 13:01:52 +0100

    expose GithubRateLimitExceeded domain error

--- api/interface_adapters/controllers/repos.py
+++ api/interface_adapters/controllers/repos.py
@@ -4,7 +4,7 @@ from api.domain.entities import Neighbour
 from api.infrastructure.external_services.github import GithubService
 from api.interface_adapters.gateways.user import get_current_active_user
 from api.usecases.get_starneighbours import get_starneighbours as get_starneighbours_usecase
-from api.domain.errors import GithubRepositoryNotFound
+from api.domain.errors import GithubRepositoryNotFound, GithubRateLimitExceeded
 
 router = APIRouter(
     prefix="/repos",
@@ -16,13 +16,14 @@ router = APIRouter(
 # TODO: remove MAX_USERS limitation in future version
 MAX_USERS = 1
 
-@router.get("/{owner}/{name}/starneighbours", response_model=list[Neighbour] | None)
-def get_starneighbours(owner: str, name: str):
+@router.get("/{user}/{repo}/starneighbours", response_model=list[Neighbour] | None)
+def get_starneighbours(user: str, repo: str):
     try:
-        neighbours = get_starneighbours_usecase(owner, name, MAX_USERS, GithubService())
+        neighbours = get_starneighbours_usecase(user, repo, MAX_USERS, GithubService())
     except GithubRepositoryNotFound:
         raise HTTPException(status_code=404, detail="This repository does not exist on github")
+    except GithubRateLimitExceeded:
+        raise HTTPException(status_code=500, detail="Reached Github Rate Limitation (this error should not be exposed, not even reached)")    
     except Exception as e:
-        print(e)
         raise HTTPException(status_code=500, detail="Internal Error, we'll investigate")
     return neighbours
