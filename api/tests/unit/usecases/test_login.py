02c4d905c27a325faebbfad5a9b4a2defc74e857
Cedric <artigue.cedric@gmail.com>
2025-01-06 10:00:43 +0100

    add login usecase unit test

--- api/tests/unit/usecases/test_login.py
+++ api/tests/unit/usecases/test_login.py
@@ -1,5 +1,18 @@
+from api.domain.errors import InvalidUserNameOrPassword
+from api.usecases.login import login
 import pytest # type: ignore
 
 @pytest.mark.unit
 def test_login():
-    assert 1 != 0
+    # test success path
+    auth = login('johndoe', 'secret')
+    assert auth["access_token"] == 'johndoe'
+    assert auth["token_type"] == 'bearer'
+
+    # test error path
+    try:
+        auth = login('should', 'fail')
+        assert False
+    except InvalidUserNameOrPassword:
+        assert True        
+
--- api/usecases/login.py
+++ api/usecases/login.py
@@ -1,10 +1,14 @@
 from api.domain.errors import InvalidUserNameOrPassword
 from api.interface_adapters.gateways.user import UserInDB, fake_users_db
 
+class AuthToken:
+    access_token: str
+    token_type: str
+
 def fake_hash_password(password: str):
     return "fakehashed" + password
 
-def login(username: str, password: str):
+def login(username: str, password: str) -> AuthToken:
     user_dict = fake_users_db.get(username)
     if not user_dict:
         raise InvalidUserNameOrPassword    
