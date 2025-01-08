1cfdee85ca9336a0db8fd28d439c9019e6b4e19d
Cedric <artigue.cedric@gmail.com>
2025-01-08 09:57:26 +0100

    unique integration test

--- api/tests/integration/test_api.py
+++ api/tests/integration/test_api.py
@@ -1,15 +1,46 @@
+import os
 import time
+import alembic.config
+from fastapi.testclient import TestClient
 import pytest # type: ignore
-from starlette.testclient import TestClient
-
 from api.main import app
+from sqlalchemy.orm import sessionmaker
 
-@pytest.fixture(scope="module")
+from api.domain.repositories import IUserRepository
+from api.infrastructure.database.client import SQL_BASE, get_engine
+from api.interface_adapters.gateways.user import InMemoryUserRepository, SQLUserRepository, UserInDB
+
+@pytest.fixture
+def fake_user_repository():
+    return InMemoryUserRepository()
+
+@pytest.fixture
+def user_repository():
+    time.sleep(1)
+    alembic.config.main(argv=["--raiseerr", "upgrade", "head"])
+    engine = get_engine(os.getenv("DB_STRING"))
+    session = sessionmaker(bind=engine)()
+    yield SQLUserRepository(session)
+    session.close()
+    # sessionmaker(bind=engine, autocommit=True)().execute(
+    #     ";".join([f"TRUNCATE TABLE {t} CASCADE" for t in SQL_BASE.metadata.tables])
+    # )
+
+@pytest.mark.integration
+def test_contract_test(fake_user_repository: IUserRepository, user_repository: IUserRepository):
+    for repository in [fake_user_repository, user_repository]:
+        repository.save(UserInDB(username="janedoe", hashed_password="secret2", disabled=False))
+
+        new_user = repository.get_by_username("janedoe")
+        assert new_user and new_user.username == "janedoe"
+
+# testing the api
+@pytest.fixture()
 def client():
     with TestClient(app) as c:
       yield c
 
-@pytest.fixture(scope="module")
+@pytest.fixture()
 def test_user():
     return {"username": "johndoe", "password": "secret"}
 
